a9db9f3324202eb3d02ff155e2ecfe2fb1f7327f
rikuto-mania <rikutoland55@gmail.com>
2026-03-10 18:36:23 +0900

    [update]フッターxリンク追加

--- app/component/footer.tsx
+++ app/component/footer.tsx
@@ -5,12 +5,13 @@ import { FaXTwitter } from "react-icons/fa6";
 
 export const Footer = () =>{
     return(
-        <footer className="bg-[#373737] py-3">
+        <footer className="bg-[#373737] py-3 text-white">
             <div className="flex gap-4 justify-center pb-2">
                 <Link href="https://github.com/rikuto-mania" ><FaGithub size={24}/></Link>
+                <Link href={"https://x.com/r__35_6"}><FaXTwitter size={24} /></Link>
                 <Link href="https://www.facebook.com/people/%E6%B2%B3%E9%87%8E%E9%99%B8%E4%BA%BA/100050814615032/"><FaFacebook size={24}/></Link>
             </div>
-            <p className="text-sm text-center">&copy;rikuto-mania All right Reserved.</p>
+            <p className="text-sm text-center">&copy; rikuto-mania All right Reserved.</p>
         </footer>
     );
 }
